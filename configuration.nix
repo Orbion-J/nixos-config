4f655601be636debcf75edd13766eaa459b3c5ca
Robin <juder.orbion@gmail.com>
2025-09-03 14:55:32 +0200

    first nixos config

---
+++ configuration.nix
@@ -0,0 +1,189 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Automatic upgrade (via channel)
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
+  
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "lama-e121"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  console = {
+     font = "Lat2-Terminus16";
+     # keyMap = "fr";
+     useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+
+    displayManager.lightdm.greeters.mini = { 
+      enable = true;
+      user = "robin";
+      extraConfig = ''
+        [greeter]
+	show-password-label = false
+	[greeter-theme]
+	background-image = "/home/robin/Images/ours_blanc_dodo_ourson.jpg"
+      '';
+    };
+
+    windowManager.i3.enable = true;
+
+    # Configure keymap in X11
+    xkb.layout = "fr";
+    # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  services.picom.enable = true;
+
+
+
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # services.pipewire.enable = false;
+  # OR
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.robin = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel" # Enable ‘sudo’ for the user.
+      "audio"
+      "networkmanager"
+    ];
+    packages = with pkgs; [
+      tree
+      firefox	# browser
+      zathura	# pdf viewer
+      nomacs	# image viewer
+      vlc	# video viewer
+      rclone	# cloud
+      lazygit	# gitutils
+      libreoffice	# bureautique
+      pulseaudio	# sound management
+      pavucontrol	# sound management
+      
+      zotero	# bibliography
+      typst	# Typst
+      texlive.combined.scheme-full	# LaTeX
+
+      python3	# Python
+    ];
+  };
+
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+     # editors
+     vim
+     neovim
+     # utils
+     wget
+     htop
+     killall
+     git
+     # environment
+     kitty	# terminal
+     polybarFull	# bar
+     nitrogen	# wallpaper
+     flameshot	# snapshot
+     arandr
+  ];
+
+  environment.variables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+    BROWSER = "firefox";
+    TERMINAL = "kitty";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
+
